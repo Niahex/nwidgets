b3987736ef32b1aedafadb2d2f9dc049446570d3
Niahex <tqm6fa6b@duck.com>
2025-11-17 14:40:19 +0100

    Update mod.rs

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,11 +1,11 @@
-pub mod hyprland;
-pub mod pipewire;
 pub mod capslock;
+pub mod hyprland;
 pub mod numlock;
+pub mod pipewire;
 pub mod pomodoro;
 
-pub use hyprland::HyprlandService;
-pub use pipewire::PipeWireService;
 pub use capslock::CapsLockService;
+pub use hyprland::HyprlandService;
 pub use numlock::NumLockService;
+pub use pipewire::PipeWireService;
 pub use pomodoro::{PomodoroService, PomodoroState};
